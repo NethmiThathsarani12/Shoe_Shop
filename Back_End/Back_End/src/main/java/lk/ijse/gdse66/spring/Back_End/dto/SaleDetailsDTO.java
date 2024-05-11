fe27e664451b9ea98604d5e6d0c84e63569d85c9
Nethmi Thathsarani <thathsaraninethmi942@gmail.com>
2024-05-11 22:52:12 +0530

    update'

--- Back_End/Back_End/src/main/java/lk/ijse/gdse66/spring/Back_End/dto/SaleDetailsDTO.java
+++ Back_End/Back_End/src/main/java/lk/ijse/gdse66/spring/Back_End/dto/SaleDetailsDTO.java
@@ -1,4 +1,19 @@
 package lk.ijse.gdse66.spring.Back_End.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class SaleDetailsDTO {
+
+    private String oId;
+    private String code;
+    private int qty;
+    private double unitPrice;
+
 }
--- Back_End/Back_End/src/main/java/lk/ijse/gdse66/spring/Back_End/dto/SalesDTO.java
+++ Back_End/Back_End/src/main/java/lk/ijse/gdse66/spring/Back_End/dto/SalesDTO.java
@@ -1,11 +1,20 @@
 package lk.ijse.gdse66.spring.Back_End.dto;
 
 import lk.ijse.gdse66.spring.Back_End.entity.Customer;
+import lk.ijse.gdse66.spring.Back_End.entity.SaleDetails;
 import lk.ijse.gdse66.spring.Back_End.enums.Payment;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class SalesDTO {
 
     private String oId;
@@ -14,7 +23,7 @@ public class SalesDTO {
     private Payment paymentMethod;
     private Integer totalPoints;
     private String cashier;
-    private String customerName;
+    private Customer customerName;
 
-    private List<SaleDetailsDTO> saleDetails = new ArrayList<>();
+    private List<SaleDetails> saleDetails = new ArrayList<>();
 }
