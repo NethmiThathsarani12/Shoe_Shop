efe51fa1c2252da02131c3d5e2a62e3a9c35bc94
Nethmi Thathsarani <thathsaraninethmi942@gmail.com>
2024-05-13 23:27:58 +0530

    update

--- Fronte_End/controller/OrderCotroller.js
+++ Fronte_End/controller/OrderCotroller.js
@@ -115,3 +115,132 @@ $("#Item_Code").click(function () {
     })
 });
 
+function updateDateTime() {
+    let currentDateTime = new Date();
+
+    let year = currentDateTime.getFullYear();
+    let month = currentDateTime.getMonth() + 1;
+    let day = currentDateTime.getDate();
+
+    let hours = currentDateTime.getHours();
+    let minutes = currentDateTime.getMinutes();
+    let seconds = currentDateTime.getSeconds();
+
+    let formattedDate = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
+    let formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+
+    $('#oDate').val(`${formattedDate} ${formattedTime}`);
+}
+
+updateDateTime();
+
+setInterval(updateDateTime, 1000);
+
+
+let itemCode;
+let itemName;
+let itemPrice;
+let itemQty;
+let itemOrderQty;
+
+let total = 0;
+let discount = 0;
+let subTotal = 0;
+
+let tableRow = [];
+$("#btnAddToCart").on("click", function () {
+
+    let duplicate = false;
+    for (let i = 0; i < $("#tblAddToCart tr").length; i++) {
+        if ($("#Item_Code option:selected").text() === $("#tblAddToCart tr").children(':nth-child(1)')[i].innerText) {
+            duplicate = true;
+
+        }
+    }
+    if (duplicate !== true) {
+
+        loadCartTableDetail();
+        reduceQty($("#buyQty").val());
+        calcTotal($("#buyQty").val() * $("#itemPrice").val());
+        $('#Item_Code,#itemName,#itemPrice,#qtyOnHand,#buyQty').val("");
+        $("#btnAddToCart").attr('disabled', false);
+    } else if (duplicate === true) {
+
+        manageQtyOnHand(tableRow.children(':nth-child(4)').text(), $("#buyQty").val());
+        $(tableRow).children(':nth-child(4)').text($("#buyQty").val());
+
+        manageTotal(tableRow.children(':nth-child(5)').text(), $("#buyQty").val() * $("#itemPrice").val());
+        $(tableRow).children(':nth-child(5)').text($("#buyQty").val() * $("#itemPrice").val());
+
+    }
+
+    $("#tblAddToCart>tr").click('click', function () {
+
+        tableRow = $(this);
+        let itemCode = $(this).children(":eq(0)").text();
+        let itemName = $(this).children(":eq(1)").text();
+        let unitPrice = $(this).children(":eq(2)").text();
+        let qty = $(this).children(":eq(3)").text();
+        let total = $(this).children(":eq(4)").text();
+
+        $("#Item_Code").val(itemCode);
+        $("#itemName").val(itemName);
+        $("#itemPrice").val(unitPrice);
+        $("#buyQty").val(qty);
+        $("#txtTotal").val(total);
+
+    });
+});
+
+function reduceQty(orderQty) {
+    let minQty = parseInt(orderQty);
+    let reduceQty = parseInt($("#qtyOnHand").val());
+    reduceQty = reduceQty - minQty;
+    $("#qtyOnHand").val(reduceQty);
+}
+
+function calcTotal(amount) {
+    total += amount;
+    $("#txtTotal").val(total);
+}
+
+function manageQtyOnHand(preQty, nowQty) {
+    var preQty = parseInt(preQty);
+    var nowQty = parseInt(nowQty);
+    let avaQty = parseInt($("#qtyOnHand").val());
+
+    avaQty = avaQty + preQty;
+    avaQty = avaQty - nowQty;
+
+    $("#qtyOnHand").val(avaQty);
+}
+
+function manageTotal(preTotal, nowTotal) {
+    total -= preTotal;
+    total += nowTotal;
+
+    $("#txtTotal").val(total);
+}
+
+$("#tblAddToCart").empty();
+function loadCartTableDetail() {
+    itemCode = $("#Item_Code").val();
+    itemName = $("#itemName").val();
+    itemPrice = $("#itemPrice").val();
+    itemQty = $("#qtyOnHand").val();
+    itemOrderQty = $("#buyQty").val();
+
+    let total = itemPrice * itemOrderQty;
+    let row = `<tr><td>${itemCode}</td><td>${itemName}</td><td>${itemPrice}</td><td>${itemOrderQty}</td><td>${total}</td></tr>`;
+
+    $("#tblAddToCart").append(row);
+}
+
+$(document).on("change keyup blur", "#txtDiscount", function () {
+    discount = $("#txtDiscount").val();
+    discount = (total / 100) * discount;
+    subTotal = total - discount;
+
+    $("#txtSubTotal").val(subTotal);
+});
+
